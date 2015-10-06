49475b45c8f8c00f40f8a1a8805ce6671680b670
Masaya Takeda <844196@gmail.com>
2015-10-07 08:09:55 +0900

    エンコード部実装
    とりあえずテストは通る

--- lib/n_cipher.rb
+++ lib/n_cipher.rb
@@ -1,5 +1,28 @@
 require "n_cipher/version"
 
 module NCipher
-  # Your code goes here...
+  module Helper
+    def is_empty?(object)
+      object.to_s.empty?
+    end
+
+    def convert_table(string, mode)
+      table = string.split('').map.with_index(0) {|c,i| [i.to_s, c] }.to_h
+      case mode
+      when :encode then table
+      when :decode then table.invert
+      end
+    end
+  end
+
+  class << self
+    include NCipher::Helper
+
+    def encode(string, seed: 'にゃんぱす', delimiter: '〜')
+      raise ArgumentError if is_empty?(string) or is_empty?(seed) or is_empty?(delimiter) or seed.size > 10
+      string.unpack('U*').map {|c| c.to_s(seed.size).gsub(/./, convert_table(seed, :encode)) }.join(delimiter)
+    end
+  end
+
+  private_class_method :is_empty?, :convert_table
 end
