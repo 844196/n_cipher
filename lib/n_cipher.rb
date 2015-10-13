1a34538945adcf033879f2ebc1bc73b81fc274a0
Masaya Takeda <844196@gmail.com>
2015-10-14 08:46:10 +0900

    encode部のドキュメント作成

--- lib/n_cipher.rb
+++ lib/n_cipher.rb
@@ -27,6 +27,30 @@ module NCipher
   class << self
     include NCipher::Helper
 
+    # 文字列を暗号化
+    #
+    # @example
+    #   NCipher.encode('abc') # 文字列のみ
+    #   #=> "ぱすん〜ぱすぱ〜ぱすす〜"
+    #
+    #   NCipher.encode('abc', seed: 'うどん') # シード値を指定
+    #   #=> "どうどんど〜どうどんん〜どうんうう〜"
+    #
+    #   NCipher.encode('abc', seed: 'うどん', delimiter: 'ひげ') # シード値、区切り文字を指定
+    #   #=> "どうどんどひげどうどんんひげどうんううひげ"
+    #
+    # @param [String] string 暗号対象文字列
+    # @param [String] seed シード値
+    # @param [String] delimiter 区切り文字
+    #
+    # @return [String] 暗号化されたStringオブジェクト
+    #
+    # @raise [ArgumentError] 引数が不正な場合
+    #   - 引数チェック項目については{Helper#common_argument_check}を参照
+    # @raise [TypeError] 文字列オブジェクト以外が渡された場合
+    #
+    # @see Helper#common_argument_check
+    # @see Helper#convert_table
     def encode(string, seed: 'にゃんぱす', delimiter: '〜')
       common_argument_check(string, seed, delimiter)
 
